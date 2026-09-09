db2b350bd5b7ad9961bc4ab1b71f1c7fcc22e60b
Akash Yadav <ay6422853@gmail.com>
2026-09-09 23:18:22 +0530

    Initial

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
-import Header from "../components/header/Header";
-import Sidebar from "../components/sidebar/Sidebar";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 import "./AppLayout.css";
 
